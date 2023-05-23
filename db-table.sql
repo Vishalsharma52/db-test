e319d98e487277b42b1e0dde92d8ec3d3c141829
Vishalsharma52 <64665000+Vishalsharma52@users.noreply.github.com>
2023-05-23 17:46:05 +0530

    Update db-table.sql

--- db-table.sql
+++ db-table.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
-CREATE DATABASE test1;
 --changeset public:03 
+CREATE DATABASE test1;
 CREATE TABLE FirstTable
 (
-    [Name] VARCHAR(100) NOT NULL,
+    [Name] VARCH(100) NOT NULL,
     [Email] VARCHAR(255) NOT NULL,
     [MobileNumber] VARCHAR(20) NULL,
     [DOB] DATE NULL
