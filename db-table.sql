fbf2bae53f7835a0a7dbcead43381cc6aab67f4b
Vishalsharma52 <64665000+Vishalsharma52@users.noreply.github.com>
2023-05-24 18:32:12 +0530

    Update db-table.sql

--- db-table.sql
+++ db-table.sql
@@ -1,9 +1,7 @@
 --liquibase formatted sql
---changeset Vishal:01
-CREATE DATABASE test1;
 
---changeset Vishal:02
-USE test1;
+--changeset Vishal:01
+USE dbtest;
 CREATE TABLE FirstTable
 (
     [Name] VARCHAR(100) NOT NULL,
@@ -12,3 +10,10 @@ CREATE TABLE FirstTable
     [DOB] DATE NULL
 );
 
+--changeset Vishal:02
+
+INSERT INTO FirstTable ([Name], [Email], [MobileNumber], [DOB])
+VALUES
+    ('John Doe', 'johndoe@example.com', '1234567890', '1990-01-01'),
+    ('Jane Smith', 'janesmith@example.com', '9876543210', '1995-02-15'),
+     ;
