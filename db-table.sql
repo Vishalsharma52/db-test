4827b155f4638dbba15017bead530c3e14432076
Vishalsharma52 <64665000+Vishalsharma52@users.noreply.github.com>
2023-05-24 18:46:32 +0530

    Update db-table.sql

--- db-table.sql
+++ db-table.sql
@@ -5,6 +5,7 @@ USE dbtest;
 CREATE TABLE FirstTable
 (
     [Name] VARCHAR(100) NOT NULL,
+    [LastName] VARCHAR(100) NOT NULL,
     [Email] VARCHAR(255) NOT NULL,
     [MobileNumber] VARCHAR(20) NULL,
     [DOB] DATE NULL
